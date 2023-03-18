ebd4478a10e21724ed0f1a6eb3bfc4cce23747f3
xSilence8x <50072173+xSilence8x@users.noreply.github.com>
2023-03-18 21:41:07 +0100

    Create logo.py

---
+++ Day7/Hangman/logo.py
@@ -0,0 +1,16 @@
+hangman_logo = """
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/                       
+"""
+about = """
+Hangman is a word-guessing game where one player thinks of a word and 
+the other player tries to guess it by suggesting letters one at a time. 
+For each incorrect guess, a part of a stick figure is drawn until the 
+entire figure is completed and the game is lost.
+"""
