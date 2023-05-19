e57897ff14be8cd95ad4433a1415194fbd695f6c
Marketa Dalecka <50072173+xSilence8x@users.noreply.github.com>
2023-05-19 21:23:08 +0200

    Add files via upload

---
+++ Day22/ball.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
